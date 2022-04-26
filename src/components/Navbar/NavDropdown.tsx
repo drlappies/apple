9e0a2bf25ab7becd15a41507070a4d59f7e94014
drlappies <yau1chun2lap3@gmail.com>
2022-04-26 21:13:10 +0800

    navbar searchbar cancel button

--- src/components/Navbar/NavDropdown.tsx
+++ src/components/Navbar/NavDropdown.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
+import NavSearch from './NavSearch'
 
 interface NavDropDownProps {
     children: JSX.Element[];
@@ -9,50 +8,10 @@ interface NavDropDownProps {
 
 const NavDropdown = (props: NavDropDownProps) => {
     const { children, isExpanded } = props;
-    const [search, setSearch] = useState<string>("");
-
-    const handleSearch = (event: any): void => {
-        event.preventDefault();
-
-        console.log("handle search")
-    }
-
-    const handleCancel = (event: any): void => {
-        event.preventDefault();
-
-        console.log("handle clear search input")
-
-        setSearch("");
-
-    }
-
-    const handleUpdate = (event: any): void => {
-        setSearch(event.target.value)
-    }
 
     return (
         <div className={`nav-dropdown-root ${isExpanded ? "expanded" : ""} `}>
-            <form className="nav-dropdown-content">
-                <div className="nav-dropdown-input-root">
-                    <FontAwesomeIcon icon={faMagnifyingGlass} className={`nav-dropdown-input-logo ${search.trim().length > 0 ? "clickable" : ""}`} onClick={handleSearch} />
-                    <input
-                        className="nav-dropdown-input"
-                        id="navbar-search-input"
-                        placeholder={"Search apple.com"}
-                        type="text"
-                        autoComplete="off"
-                        autoCapitalize="off"
-                        autoCorrect="off"
-                        spellCheck="false"
-                        value={search}
-                        onChange={handleUpdate}
-                    />
-                    <FontAwesomeIcon icon={faCircleXmark} className={`nav-dropdown-input-logo ${search.trim().length > 0 ? "clickable" : "hidden"}`} onClick={handleCancel} />
-                </div>
-                <input id="navbar-search-input" type="hidden" name="navbar-search-input" />
-                <button>Cancel</button>
-            </form>
-
+            <NavSearch />
             {children}
         </div>
     )
--- src/components/Navbar/NavSearch.tsx
+++ src/components/Navbar/NavSearch.tsx
@@ -1,11 +1,57 @@
-import useInput from '../../hooks/useInput';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+
 
 const NavSearch = () => {
-    const [query, handleQueryChange] = useInput("");
+    const [search, setSearch] = useState<string>("");
+    const [isFocus, setIsFocus] = useState<boolean>(false);
+
+    const handleSearch = (event: any): void => {
+        event.preventDefault();
+
+        console.log("handle search")
+    }
+
+    const handleCancel = (event: any): void => {
+        event.preventDefault();
+
+        console.log("handle clear search input")
+
+        setSearch("");
+
+    }
+
+    const handleUpdate = (event: any): void => {
+        setSearch(event.target.value)
+    }
+
 
     return (
-        <div>
-            <input type="text" name="query" />
+        <div className="nav-dropdown-input-root">
+            <div className={`nav-dropdown-input-container ${isFocus ? "focused" : ""}`}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} className={`nav-dropdown-input-logo ${search.trim().length > 0 ? "clickable" : ""}`} onClick={handleSearch} />
+                <input
+                    className="nav-dropdown-input"
+                    id="navbar-search-input"
+                    placeholder={"Search apple.com"}
+                    type="text"
+                    autoComplete="off"
+                    autoCapitalize="off"
+                    autoCorrect="off"
+                    spellCheck="false"
+                    value={search}
+                    onChange={handleUpdate}
+                    onFocus={() => setIsFocus(true)}
+                />
+                <input id="navbar-search-input" type="hidden" name="navbar-search-input" />
+                <FontAwesomeIcon icon={faCircleXmark} className={`nav-dropdown-input-logo ${search.trim().length > 0 ? "clickable" : "hidden"}`} onClick={handleCancel} />
+
+            </div>
+            {isFocus && <button onClick={() => setIsFocus(false)} className="nav-dropdown-input-cancel-btn-container">
+                <span className="nav-dropdown-input-cancel-btn">Cancel</span>
+            </button>}
+
         </div>
     )
 }
